15eab7be4118a6fb99cf623de9e09ee9ea28d440
mushan <mushan.zb@gmail.com>
2022-08-27 23:59:34 +0800

    fix: 修改启动脚本

--- script/run.sh
+++ script/run.sh
@@ -1,5 +1,11 @@
-mkdir output
-cp -rf ../config config
-cp -rf ../file file
+baseDir = $(cd $(dirname $0);pwd)/..
+output = $baseDir/output
 
-go build -o rankland ../.
+if [! -d output]; then
+    mkdir output
+
+cp -rf $baseDir/config $output/config
+cp -rf $baseDir/file $output/file
+
+go build -o $output/rankland $baseDir
+sh $output/rankland
