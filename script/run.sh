0ccbccb310b33f4a5b9e3e52a195f3e5feac660f
mushan <mushan.zb@gmail.com>
2022-08-28 00:24:09 +0800

    fix: 修复脚本不能启动问题

--- script/run.sh
+++ script/run.sh
@@ -1,8 +1,13 @@
-baseDir = $(cd $(dirname $0);pwd)/..
-output = $baseDir/output
+baseDir=$(cd $(dirname $0); cd ..; pwd)
+# echo $baseDir
+output=$baseDir/output
+# echo $output
 
-if [! -d output]; then
-    mkdir output
+
+
+if [ ! -d $output ];then
+    mkdir $output
+fi
 
 cp -rf $baseDir/config $output/config
 cp -rf $baseDir/file $output/file
