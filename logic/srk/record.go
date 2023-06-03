c7d029d639cbe1e1bbd399b2c4b180c350e746e3
mushan <mushan.zb@gmail.com>
2023-06-03 23:53:25 +0800

    feat: 提交 id 改成 string

--- logic/srk/record.go
+++ logic/srk/record.go
@@ -1,7 +1,7 @@
 package srk
 
 type Record struct {
-	ID             int64  `json:"id"` // 一个标记提交顺序的唯一 ID
+	ID             int64  `json:"id,string"` // 一个标记提交顺序的唯一 ID
 	MemberID       string `json:"memberID"`
 	ProblemID      string `json:"problemID"`
 	Result         string `json:"result"`
